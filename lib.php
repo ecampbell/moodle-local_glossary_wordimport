c78868b4b97b67c469857794b50f1f79366979dd
Eoin Campbell <campbeeo@tcd.ie>
2020-06-30 15:47:01 +0100

    Finally got import/export links added

--- lib.php
+++ lib.php
@@ -24,56 +24,44 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-global $CFG;
-require_once($CFG->dirroot . '/lib/editor/atto/plugins/wordimport/xslemulatexslt.inc');
-require_once($CFG->libdir . '/xmlize.php');
-
-/**
- * General definitions
- */
 
 /**
  * Add import/export commands to the Glossary settings block
  *
- * @param navigation_node $navigation The navigation node to extend
- * @param stdClass $course The course to object for the report
- * @param stdClass $context The context of the course
+ * @param settings_navigation $settings The settings navigation object
  */
+function local_glossary_wordimport_extend_settings_navigation(settings_navigation $settings) {
+    global $PAGE, $DB, $CFG, $USER;
 
-function local_glossary_wordimport_extend_navigation_course($navigation, $course, $context) {
-    global $PAGE;
+    $mode = optional_param('mode', '', PARAM_ALPHA);
+    $hook = optional_param('hook', 'ALL', PARAM_CLEAN);
 
+    // Do nothing when installing the plugin.
     if (!$PAGE->cm || $PAGE->cm->modname !== 'glossary') {
         return;
     }
 
-    $params = $PAGE->url->params();
-    if (empty($params['id']) and empty($params['cmid'])) {
-        return;
+    // Change null_progress_trace to html_progress_trace for debugging.
+    $trace = new null_progress_trace();
+    $trace->output("local_glossary_wordimport_extend_settings_navigation()");
+
+    // Use the permissions context to decide whether to add custom links to the activity settings.
+    $context = \context_module::instance($PAGE->cm->id);
+
+    // Get the the activity settings menu node from the activity node.
+    $menu = $settings->find('modulesettings', settings_navigation::TYPE_SETTING);
+    $trace->output("menu: key = " . $menu->key . "; text = " . $menu->text, 1);
+
+    if (has_capability('mod/glossary:import', $context)) {
+       $url1 = new moodle_url('/local/glossary_wordimport/index.php', array('id' => $PAGE->cm->id, 'action' => 'import', 'mode' => $mode, 'hook' => $hook));
+        $menu->add(get_string('wordimport', 'local_glossary_wordimport'), $url1, navigation_node::TYPE_SETTING, null, null,
+               new pix_icon('f/document', '', 'moodle', array('class' => 'iconsmall', 'title' => '')));
     }
 
-    if (empty($PAGE->cm->context)) {
-        $PAGE->cm->context = get_context_module::instance($PAGE->cm->instance);
-    }
-
-    if (!(has_capability('mod/glossary:manageentries', $PAGE->cm->context) and
-        has_capability('mod/glossary:import', $PAGE->cm->context))) {
-        return;
-    }
-
-    // Configure Import link, and pass in the current glossary in case the insert should happen here rather than at the end.
-    if (has_capability('mod/glossary:import', $PAGE->cm->context)) {
-        $url1 = new moodle_url('/local/glossary_wordimport/index.php', array('id' => $PAGE->cm->id, 'action' => 'import'));
-        $navigation->add(get_string('wordimport', 'local_glossary_wordimport'), $url1, navigation_node::TYPE_SETTING, null, null,
-                new pix_icon('f/document', '', 'moodle', array('class' => 'iconsmall', 'title' => '')));
-    }
-
-    // Configure Export links for current glossary.
-    if (has_capability('mod/glossary:export', $PAGE->cm->context)) {
-        $url2 = new moodle_url('/local/glossary_wordimport/index.php', array('id' => $PAGE->cm->id, 'action' => 'export'));
-        $navigation->add(get_string('wordexport', 'local_glossary_wordimport'), $url2, navigation_node::TYPE_SETTING,
-            null, null, new pix_icon('f/document', '', 'moodle', array('class' => 'iconsmall', 'title' => '')));
+    if (has_capability('mod/glossary:export', $context)) {
+        $url2 = new moodle_url('/local/glossary_wordimport/index.php', array('id' => $PAGE->cm->id, 'action' => 'export', 'mode' => $mode, 'hook' => $hook));
+        $menu->add(get_string('wordexport', 'local_glossary_wordimport'), $url2, navigation_node::TYPE_SETTING,
+           null, null, new pix_icon('f/document', '', 'moodle', array('class' => 'iconsmall', 'title' => '')));
     }
 
 }
-
