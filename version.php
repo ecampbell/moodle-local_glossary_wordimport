c032848fc65358084f48e1eeee3dda7569ce3abf
Eoin Campbell <campbeeo@tcd.ie>
2025-04-23 16:28:26 +0100

    Fix code styling issues, support export of img/@alt attribute

--- version.php
+++ version.php
@@ -24,9 +24,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2025041500;              // The current plugin version (Date: YYYYMMDDXX).
-$plugin->requires = 2025041400;               // Requires Moodle 5.0 or higher.
+$plugin->version = 2025042300;              // The current plugin version (Date: YYYYMMDDXX).
+$plugin->requires = 2020060900;             // Requires Moodle 3.9 or higher.
 $plugin->component = 'local_glossary_wordimport';   // Full name of the plugin (used for diagnostics).
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '1.1.7 (Build: 2025041500)'; // Human readable version information.
-$plugin->dependencies = ['booktool_wordimport' => 2025041501]; // Requires Book Word import plugin v1.4.13.
+$plugin->release = '1.1.7 (Build: 2025042300)'; // Human readable version information.
+$plugin->dependencies = ['booktool_wordimport' => 2025042300]; // Requires Book Word import plugin.
